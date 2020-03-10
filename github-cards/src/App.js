aaa849617cf14ae1528c8316d73d00856e0f367b
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-10 14:10:53 -0700

    styling in progress

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,32 +1,65 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios'
-import UserCard from './Components/UserCard';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import UserCard from "./Components/UserCard";
 
 class App extends React.Component {
-
   state = {
-    user: {}
-  }
+    user: {},
+    userFollowers: [],
+    followersData: []
+  };
 
   componentDidMount() {
     axios(`https://api.github.com/users/SyriiAdvent`)
-      .then(response => {
-        console.log(response.data)
-        this.setState({
-          user: response.data
+    .then(response => {
+      // console.log(response.data)
+      this.setState({
+        user: response.data
+      });
+    })
+    .catch(error => console.log("error: ", error))
+    .then(() => {
+      axios(`https://api.github.com/users/SyriiAdvent/followers`)
+        .then(response => {
+          // console.log("followers data ", response.data);
+          this.setState({
+            userFollowers: response.data
+          });
         })
+        .catch(error => console.log("error: ", error));
+    })
+    .catch(error => console.log("error: ", error))
+
+    // console.log(this.state.userFollowers)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('CDU is Running')
+    if(prevState.userFollowers !== this.state.userFollowers) {
+      this.state.userFollowers.forEach(ele => {
+        // console.log(ele)
+        axios(`${ele.url}`)
+        .then(response => {
+          console.log(response.data)
+          this.setState({
+            followersData: [...this.state.followersData, response.data]
+          })
+        })
+        .catch(error => console.log(error))
       })
+    }
   }
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <UserCard user={this.state.user} />
+        {/* {console.log(this.state.userFollowers)} */}
+        {this.state.followersData.map((items) => <UserCard key={items.node_id} user={items} />)}
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
--- github-cards/src/Components/UserCard.js
+++ github-cards/src/Components/UserCard.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './UserCard.css'
 
-const UserCard = (props) => {
+const UserCard = ({ user }) => {
   return (
-    <div className='Card-Container'>
-      
+    <div className='cardContainer'>
+        <img className='userImg' src={user.avatar_url} alt={user.name} />
+      <div className='cardInfo'>
+        <h3>{user.name}</h3>
+        <h4>{user.login}</h4>
+
+        <p>Location: {user.location}</p>
+        <p>Profile: <a href={user.html_url}>{user.html_url}</a></p>
+        <p>Followers: {user.followers}</p>
+        <p>Following: {user.following}</p>
+        <p>{user.blog}</p>
+      </div>
     </div>
   )
 }
